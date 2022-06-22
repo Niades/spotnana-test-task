27e140db6514bbd7b310fadfbfd212e70d0a737c
Daniil Polovinkin <dolovinkin@gmail.com>
2022-06-22 19:38:32 +0400

    feate(components/button): add button to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import './App.css'
+import { Button } from '../src/components/button';
 
 function App() {
-  return <div className="App"></div>
+  return <div className="App">
+    <Button>Press me!</Button>
+  </div>
 }
 
 export default App
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,16 +1,12 @@
-import { useCallback } from 'react'
 import type { ReactNode } from 'react'
 
 type ButtonProps = {
   children: ReactNode
 }
 
-function Button(props: ButtonProps) {
-  const { children } = props
-  const onClick = useCallback<(e: Event) => void>((e) => e.preventDefault(), [])
+const Button: React.FC<ButtonProps> = (props) => {
   return (
-    // @ts-expect-error
-    <Button onClick={onClick}>{children}</Button>
+    <button type="button">{props.children}</button>
   )
 }
 
